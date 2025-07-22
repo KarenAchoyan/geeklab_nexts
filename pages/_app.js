a2263839a7ad0bb2027da53930582cae78f71dd2
Karen <karenachoyan8@gmail.com>
2025-07-22 19:46:11 +0400

    Changes

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,6 @@ import hy from '../locales/hy.json';
 import en from '../locales/en.json';
 import ge from '../locales/ge.json';
 import {useRouter} from "next/router";
-import '@formatjs/intl-numberformat/polyfill';
-import '@formatjs/intl-numberformat/locale-data/ka'; // Georgian locale
 
 const messages = {
     hy,
